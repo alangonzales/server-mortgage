529b21fa9ad2a2ebc9542c29fbe9d8f887181ec7
alan <alan.gonzales.jobs@gmail.com>
2019-09-28 17:45:46 +0100

    prueba

--- facade/facade.js
+++ facade/facade.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+// parse application/json      
 app.use(bodyParser.json())
 
 
